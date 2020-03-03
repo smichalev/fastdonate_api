aeed7bac11d51063ade2b92665b888cd9ed397e9
smichalev <Xx1820082916>
2020-03-03 21:11:59 +0300

    авторизация

--- routes/auth/get.steam.js
+++ routes/auth/get.steam.js
@@ -4,6 +4,6 @@ module.exports = (router) => {
 	router.get('/steam', passport.authenticate('steam', {failureRedirect: '/'}), request);
 };
 
-let request = (req, res, next) => {
+function request(req, res, next) {
 	res.redirect('/');
 };
